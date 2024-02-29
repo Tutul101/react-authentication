ab072e89577d57ea41a0e798d23d0229a8b86eaf
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-02-29 08:28:32 +0530

    update

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,43 @@
-import AuthForm from '../components/AuthForm';
+import AuthForm from "../components/AuthForm";
+import { json, redirect } from "react-router-dom";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export const action = async ({ request }) => {
+  const searchParams = new URL(request.url).searchParams;
+
+  const mode = searchParams.get("mode") || "login";
+  console.log("mode", mode);
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsupported mode" }, { status: 422 });
+  }
+  const data = await request.formData();
+
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+  console.log("authData", authData);
+  const response = fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  // if (response.status === 422 || response.status === 401) {
+  //   return response;
+  // }
+  // if (!response.ok) {
+  //   throw json({ message: "Could not authenticate user. " }, { status: 500 });
+  // }
+
+  // We have to manage the token
+
+  return redirect("/");
+};
